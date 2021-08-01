e39147287ea14386cb212a3955531c8650985db0
Dmitry Velichko <soleil-2005@mail.ru>
2021-08-01 18:24:46 +0300

    New commit

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -7,6 +7,14 @@ const AddTask = () => {
                 <label>Task</label>
                 <input type="text" placeholder="Add Task"/>
             </div>
+            <div className="form-control">
+                <label>Day & Time</label>
+                <input type="text" placeholder="Add Day & Time"/>
+            </div>
+            <div className="form-control">
+                <label>Set Reminder</label>
+                <input type="checkbox"/>
+            </div>
         </form>
     )
 }
