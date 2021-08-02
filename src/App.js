9402dec279de9944e80d0b6a16f54f509900d94b
Dmitry Velichko <soleil-2005@mail.ru>
2021-08-02 12:17:22 +0300

    New commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,8 @@ function App() {
   // Add Task
 
   const addTask = (task) => {
-    console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1;
+    console.log(id);
   }
 
   // Delete Task
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -17,7 +17,7 @@ const AddTask = ({ onAdd }) => {
 
         setText('')
         setDay('')
-        setReminder('')
+        setReminder(false)
 
     }
 
@@ -33,7 +33,7 @@ const AddTask = ({ onAdd }) => {
             </div>
             <div className="form-control form-control-check">
                 <label>Set Reminder</label>
-                <input type="checkbox" value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
+                <input type="checkbox" value={reminder} checked={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
             </div>
 
             <input type="submit" value='Save Task' className='btn btn-block' />
