5e6da3adb92e03b4581b51fa0b588696fc7cf068
Dmitry Velichko <soleil-2005@mail.ru>
2021-08-04 18:14:16 +0300

    New commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
     const getTasks = async () => {
       const tasksFromServer = await fetchTasks()
       setTasks(tasksFromServer)
-      console.log(tasksFromServer)
     }
     getTasks()
   }, [])
@@ -37,7 +36,11 @@ function App() {
 
   // Delete Task
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+    
     setTasks(tasks.filter((task) => task.id !== id));
   }
 
